f51485499457fb0c5ecf329e8025641a11c243d9
TravellerOnCode <avijitchakraborty1998@gmail.com>
2020-06-29 17:04:52 +0530

    Google search

--- notably/lib/keyword_extract.dart
+++ notably/lib/keyword_extract.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:url_launcher/url_launcher.dart';
 
 String data;
 Future<Album> fetchAlbum(String title) async {
@@ -25,6 +26,7 @@ Future<Album> fetchAlbum(String title) async {
     throw Exception('Failed to fetch response');
   }
 }
+
 
 class Album {
   //final int results;
@@ -57,15 +59,60 @@ class DisplayResponse extends StatefulWidget {
 }
 
 class _DisplayResponse extends State<DisplayResponse> {
+
+  Future<void> _launched;
+//String _phone = '';
+
+Future<void> _launchInBrowser(String url) async {
+  print('launch in browser called !');
+    if (await canLaunch(url)) {
+      await launch(
+        url,
+        //forceSafariVC: false,
+        //forceWebView: false,
+        //headers: <String, String>{'my_header_key': 'my_header_value'},
+      );
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  Widget _launchStatus(BuildContext context, AsyncSnapshot<void> snapshot) {
+    if (snapshot.hasError) {
+      return Text('Error: ${snapshot.error}');
+    } else {
+      return const Text('');
+    }
+  }
+
 Future<Album> futureAlbum;
   String value;
   _DisplayResponse(this.value);
+  TextEditingController _controller;
+
+  
+  
   @override
+  /*
   void initState() { 
     super.initState();
     data = value;
     print(data);
     futureAlbum = fetchAlbum(data);
+  }*/
+  void initState() {
+    super.initState();
+    data = value;
+    print(data);
+    futureAlbum = fetchAlbum(data);
+    _controller = TextEditingController(
+      text: ""
+    );
+  }
+
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   
@@ -92,11 +139,56 @@ Future<Album> futureAlbum;
                 child: new ListView(
                   padding: const EdgeInsets.fromLTRB(20,50,10,10),
                   children: <Widget>[
-                    Text('Top Words',style: new TextStyle(fontSize: 80.0)),
-                    Text(snapshot.data.title,style: new TextStyle(fontSize: 40.0)),
+                    Text('Top Words',style: new TextStyle(fontSize: 40.0)),
+                    Text(snapshot.data.title,style: new TextStyle(fontSize: 20.0)),
                     //Text('Word Count',style: new TextStyle(fontSize: 40.0)),
                     //Text(snapshot.data.wordscount,style: new TextStyle(fontSize: 60.0)),
-
+                    Center(
+        child: TextField(
+          decoration: const InputDecoration(
+            hintText: "Type your keyword here"
+          ),
+          maxLines: null,
+          autocorrect: true,
+          controller: _controller,
+          onSubmitted: (String value) async {
+            await showDialog<void>(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: const Text('Thanks!'),
+                  content: Text('You typed "$value".'),
+                  actions: <Widget>[
+                    FlatButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: const Text('OK'),
+                    ),
+                  ],
+                );
+              },
+            );
+          },
+          ),
+        ),
+        RaisedButton(
+                onPressed: () => setState(() {
+                  Text t = Text(_controller.text);
+                   String s = t.data;
+                   String urllink = 'https://www.google.com/search?q=' +  s;
+                  _launched = _launchInBrowser(urllink);
+                }),
+                child: Text(
+              'Make a Google Search',
+              style: TextStyle (
+                fontSize: 20,
+                color: Colors.black
+              ),
+            ),
+              ),
+          
+        FutureBuilder<void>(future: _launched, builder: _launchStatus),
                   ],
                   ),
                 );
